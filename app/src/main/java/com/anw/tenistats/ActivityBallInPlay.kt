184037103a62aa28c94d9ecfdadd3ac5e5f613de
ulaneb <ul0275817@edu.uni.lodz.pl>
2024-03-21 15:54:48 +0100

    added layouts ~u

--- app/src/main/java/com/anw/tenistats/ActivityBallInPlay.kt
+++ app/src/main/java/com/anw/tenistats/ActivityBallInPlay.kt
@@ -1,31 +1,16 @@
 package com.anw.tenistats
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.firebase.Firebase
-import com.google.firebase.auth.FirebaseAuth
 
-class ActivityMenu : AppCompatActivity() {
-    private lateinit var auth : FirebaseAuth
+class ActivityBallInPlay : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_menu)
-        auth = FirebaseAuth.getInstance()
-        findViewById<Button>(R.id.buttonLogOut).setOnClickListener{
-            auth.signOut()
-            startActivity(Intent(this,MainActivity::class.java))
-        }
-
-        findViewById<Button>(R.id.buttonStartNewGame).setOnClickListener{
-            startActivity(Intent(this,StartNewActivity::class.java))
-        }
-
+        setContentView(R.layout.activity_ball_in_play)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
