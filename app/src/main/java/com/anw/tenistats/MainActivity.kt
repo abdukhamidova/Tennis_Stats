28b4ed20bc4529d5b4ed4bacd0c83e9ccb5f5626
ulaneb <ul0275817@edu.uni.lodz.pl>
2024-03-21 16:46:40 +0100

    ~u

--- app/src/main/java/com/anw/tenistats/MainActivity.kt
+++ app/src/main/java/com/anw/tenistats/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.anw.tenistats
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,6 +15,15 @@ class MainActivity : AppCompatActivity() {
         //auth = Firebase.auth
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.buttonLogIn).setOnClickListener {
+            startActivity(Intent(this,SignInActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.buttonSignUp).setOnClickListener {
+            startActivity(Intent(this,SignUpActivity::class.java))
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
         val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
         v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
